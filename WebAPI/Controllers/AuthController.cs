2949102a5335ff6d0c7e344e9d69284a8751633e
s.lachi <s_lachi@esi.dz>
2018-05-23 11:45:51 +0100

    Ajoutez des fichiers projet.

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Data;
+using WebAPI.Model;
+
+namespace WebAPI.Controllers
+{
+   
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        [HttpPost]
+        [Route("login")]
+        public async Task <IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Username);
+            if(user != null && await userManager.CheckPasswordAsync(user , model.Password))
+            {
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                };
+
+                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySuperSecureKey"));
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://www.anem.dz/",
+                    audience: "http://www.anem.dz/",
+                    expires: DateTime.UtcNow.AddHours(1),
+                    claims: claims,
+                    signingCredentials : new Microsoft.IdentityModel.Tokens.SigningCredentials(signingKey,SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
